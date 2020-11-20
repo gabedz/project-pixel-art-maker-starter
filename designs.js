d047c12ff4a6cf0e6759884cf90650b9fa405e4f
gabedz <74375652+gabedz@users.noreply.github.com>
2020-11-19 21:01:11 -0500

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,10 +1,22 @@
 // Select color input
+var color;
+
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
-
-// Your code goes here!
-
+    let grid = document.getElementById('pixelCanvas');
+    let gridWidth = document.getElementById('inputWidth');
+    let gridHeight = document.getElementById('inputHeight');
+    for (y = 0; y <= gridHeight.value; y++) {
+        var newRow = document.createElement('tr');
+        grid.appendChild(newRow);
+        for (x = 0; x <= gridWidth.value; x++) {
+            var newCol = document.createElement('td');
+            grid.appendChild(newCol);
+        }
+    }
 }
+
+document.addEventListener('submit', makeGrid());
